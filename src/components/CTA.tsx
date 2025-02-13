928fac124600579a6697c1c66f958116a9a067b0
itsmefminsaf <itsmefminsaf@gmail.com>
2025-02-13 19:28:53 +0530

    feat: removed redirection from landing page

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,24 +1,12 @@
 "use client";
 
 import { redirect } from "next/navigation";
-import { useEffect } from "react";
 
 const CTA = () => {
-  useEffect(() => {
-    if (localStorage.getItem("visited")) {
-      return redirect("/shop?category=all");
-    }
-  });
-
-  const handleClick = () => {
-    localStorage.setItem("visited", "true");
-    return redirect("/shop?category=all");
-  };
-
   return (
     <button
       onClick={() => {
-        handleClick();
+        redirect("/shop?category=all");
       }}
       className="shadow-md active:scale-95 duration-500 text-xl mt-4 relative left-1/2 -translate-x-1/2 bg-foreground text-background rounded-xl text-nowrap p-3"
     >
