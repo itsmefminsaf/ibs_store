2b0f9798c4295b9d684cc5f1ee826064596d7f10
itsmefminsaf <itsmefminsaf@gmail.com>
2025-02-17 18:22:43 +0530

    fix: gradient orientation bug fixed in feature section

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -6,8 +6,10 @@ import { featureProp } from "@/libs/type";
 const Feature = ({ img, title, description, dir = "left" }: featureProp) => {
   return (
     <div
-      className={`flex items-center justify-around m-3 rounded-e-3xl p-4 bg-gradient-to-r from-transparent to-foreground/10 ${
-        dir === "left" ? "flex-row" : "flex-row-reverse"
+      className={`flex items-center justify-around m-3 rounded-e-3xl p-4 from-transparent to-foreground/10 ${
+        dir === "left"
+          ? "flex-row bg-gradient-to-r"
+          : "flex-row-reverse bg-gradient-to-l"
       }`}
     >
       <Image
